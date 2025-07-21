6ea08f9b42d4975ff34b0560a9cc6c5aaa491517
Alec <215294797+justalec27@users.noreply.github.com>
2025-07-21 21:27:54 +0200

    Include decimal button and solved some bugs
    Users can now create numbers with decimal. Also solved few bugs, such as
    removing error when user divides by 0 or error when user presses "="
    button when no 2 numbers or an operator is given

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-let array1 = []
-let array2 = [] // Need to include multiple digits 
-
 let num1  = ""
 let operator = ""
 let num2 = ""
+let result = ""
 
 
 function operate(num1, operator, num2){
@@ -18,134 +16,417 @@ function operate(num1, operator, num2){
     } 
 }
 
-console.log(operate(3, "+", 7))
+function isValidNumber(value) {
+    if ( value === 0 )
+        return "is zero"
+    else if (value === "")
+        return "is string"
+    else if (value === null)
+        return "is null"
+}
 
+console.log(isValidNumber(num1))
 
 //button functions
 const outcome = document.querySelector(".outcome")
 
 const clear = document.querySelector(".clear")
 clear.addEventListener("click", (event) => {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    result = "";
     outcome.textContent = ""
-    array1 = []
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
+
+})
+
+const decimal = document.querySelector(".decimal")
+decimal.addEventListener("click", (event) => {
+    if ( num1.includes(".") === false || num2.includes(".") === false ){
+        if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += ".";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += ".";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += ".";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
+    } else if (num1.contains(".") === true || num2.contains(".") === true){
+        ;
+    }
+
 })
 
 const zero = document.querySelector(".zero")
 zero.addEventListener("click", (event) => {
-    outcome.textContent = 0
-    array1.push(0)
-    console.log(array1)
+    if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "0";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "0";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "0";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const one = document.querySelector(".one")
 one.addEventListener("click", (event) => {
-    outcome.textContent = 1
-    array1.push(1)
-    console.log(array1)
+      if (result != ""){
+      num1 = result;
+      result = ""
+      num2 = "";
+      num2 += "1";
+      outcome.textContent = num2;
+
+    } else if (operator === ""){
+        num1 += "1";
+        outcome.textContent = num1
+    } else if (operator != "" && result === ""){
+        num2 += "1";
+        outcome.textContent = num2
+    }
+console.log(`The num1 is ${num1}`)
+console.log(`The num2 is ${num2}`)
+console.log(`The result is ${result}`)
+console.log(`The operator is ${operator}`)
 })
 
 const two = document.querySelector(".two")
 two.addEventListener("click", (event) => {
-    outcome.textContent = 2
-    array1.push(2)
-    console.log(array1)
+     if (result != ""){
+      num1 = result;
+      result = ""
+      num2 = "";
+      num2 += "2";
+      outcome.textContent = num2;
+
+    } else if (operator === ""){
+        num1 += "2";
+        outcome.textContent = num1
+    } else if (operator != "" && result === ""){
+        num2 += "2";
+        outcome.textContent = num2
+    }
+console.log(`The num1 is ${num1}`)
+console.log(`The num2 is ${num2}`)
+console.log(`The result is ${result}`)
+console.log(`The operator is ${operator}`)
 })
 
 const three = document.querySelector(".three")
 three.addEventListener("click", (event) => {
-    outcome.textContent = 3 
-    array1.push(3) 
-    console.log(array1)
+      if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "3";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "3";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "3";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const four = document.querySelector(".four")
 four.addEventListener("click", (event) => {
-    outcome.textContent = 4
-    array1.push(4)
-    console.log(array1)
+     if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "4";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "4";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "4";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const five = document.querySelector(".five")
 five.addEventListener("click", (event) => {
-    outcome.textContent = 5
-    array1.push(5)
-    console.log(array1)
+     if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "5";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "5";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "5";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const six = document.querySelector(".six")
 six.addEventListener("click", (event) => {
-    outcome.textContent = 6
-    array1.push(6)
-    console.log(array1)
+    if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "6";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "6";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "6";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)  
 })
 
 const seven = document.querySelector(".seven")
 seven.addEventListener("click", (event) => {
-    outcome.textContent = 7
-    array1.push(7)
-    console.log(array1)
+    if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "7";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "7";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "7";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const eight = document.querySelector(".eight")
 eight.addEventListener("click", (event) => {
-    outcome.textContent = 8
-    array1.push(8)
-    console.log(array1)
+     if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "8";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "8";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "8";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const nine = document.querySelector(".nine")
 nine.addEventListener("click", (event) => {
-    outcome.textContent = 9
-    array1.push(9)
-    console.log(array1)
+     if (result != ""){
+        num1 = result;
+        result = ""
+        num2 = "";
+        num2 += "9";
+        outcome.textContent = num2;
+
+        } else if (operator === ""){
+            num1 += "9";
+            outcome.textContent = num1
+        } else if (operator != "" && result === ""){
+            num2 += "9";
+            outcome.textContent = num2
+        }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const divide = document.querySelector(".divide")
 divide.addEventListener("click", (event) => {
-    array1.push("/")
-    console.log(array1)
+
+    if (operator === ""){
+        operator = "/"; 
+    } else if (operator != ""){
+        if (Number(num2) === 0) {
+            window.alert("We can not divide with a zero. \nPlease start over.")
+            num1 = "";
+            num2 = "";
+            operator = "";
+            result = "";
+        } else {
+            num1 = Number(num1)
+            num2 = Number(num2)
+            result = operate(num1, operator, num2)
+            outcome.textContent = result
+            operator = "/"
+    }}
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const multiply = document.querySelector(".multiply")
 multiply.addEventListener("click", (event) => {
-    array1.push("*")
-    console.log(array1)
+    if (operator === ""){
+        operator = "*"; 
+    } else if (operator != ""){
+        if (Number(num2) === 0 && operator === "/") {
+            window.alert("We can not divide with a zero. \nPlease start over.")
+            num1 = "";
+            num2 = "";
+            operator = "";
+            result = ""; 
+    }   else{
+        num1 = Number(num1)
+        num2 = Number(num2)
+        result = operate(num1, operator, num2)
+        outcome.textContent = result
+        operator = "*"
+    }
+ }
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const minus = document.querySelector(".minus")
 minus.addEventListener("click", (event) => {
-    array1.push("-")
-    console.log(array1)
+    if (operator === ""){
+      operator = "-"; 
+    } else if (operator != ""){
+        if (Number(num2) === 0 && operator === "/") {
+            window.alert("We can not divide with a zero. \nPlease start over.")
+            num1 = "";
+            num2 = "";
+            operator = "";
+            result = "";
+    }   else {
+            num1 = Number(num1)
+            num2 = Number(num2)
+            result = operate(num1, operator, num2)
+            outcome.textContent = result
+            operator = "-"
+    }
+}
+    console.log(`The num1 is ${num1}`)
+    console.log(`The num2 is ${num2}`)
+    console.log(`The result is ${result}`)
+    console.log(`The operator is ${operator}`)
 })
 
 const add = document.querySelector(".add")
 add.addEventListener("click", (event) => {
-    array1.push("+")
-    console.log(array1)
+    if (operator === ""){
+    operator = "+"; 
+    } else if (operator != ""){
+        if (Number(num2) === 0 && operator === "/") {
+        window.alert("We can not divide with a zero. \nPlease start over.")
+        num1 = "";
+        num2 = "";
+        operator = "";
+        result = "";
+    }   else {
+        num1 = Number(num1)
+        num2 = Number(num2)
+        result = (operate(num1, operator, num2))
+        outcome.textContent = result
+        operator = "+"
+    }
+ }
+console.log(`The num1 is ${num1}`)
+console.log(`The num2 is ${num2}`)
+console.log(`The result is ${result}`)
+console.log(`The operator is ${operator}`)
 })
+
 
 
 const equals = document.querySelector(".equals")
 equals.addEventListener("click", (event) => {
-
-    num1  = Number(array1[0]);
-    operator = array1[1];
-    num2 = Number(array1[2]);
-    let result = operate(num1, operator, num2)
-    outcome.textContent = result
+        if ( num1 === "" || num2 === "" || operator === "") {
+            window.alert("You need to enter two number and an operator. Please start again")
+            num1 = "";
+            num2 = "";
+            operator = "";
+            result = "";
+    // Prevent divide by zero
+        } else if ( Number(num2) === 0 && operator === "/") {
+            window.alert("We can not divide with a zero. \nPlease start over.")
+            num1 = "";
+            num2 = "";
+            operator = "";
+            result = "";
+        } else {
+            // Safe to calculate
+            num1 = Number(num1)
+            num2 = Number(num2)
+            result = operate(num1, operator, num2)
+            outcome.textContent = result
+         }
+        console.log(`The num1 is ${num1}`)
+        console.log(`The num2 is ${num2}`)
+        console.log(`The result is ${result}`)
+        console.log(`The operator is ${operator}`)
 })
-
-console.log(array1)
 
 
 /* 
-Note voor volgende keer:
-1. Zorg dat de rekenmachine meerdere getallen kan optellen voor num1 en num2, waarschijlijk
-heb ik daarvoor 2 arrays nodig. 
 
-2. Ga verder vanaf punt 7. De rest werkt. 
-
-
-
+Open issues:
+1. Pressing = before entering all of the numbers or an operator could cause problems! 
+    UPDATE: problems seems solved. Check again tomorrow
+2. divide by zero is also not working at .equals
 
 */
